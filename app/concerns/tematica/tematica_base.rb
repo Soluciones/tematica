096bf4ee2def2acb368bbfbe7d7e866a16877bf3
Fernan2 <fernandorankia@gmail.com>
2014-02-13 17:20:47 +0100

    Tematica is now a concern, so we can handle it in the host app

---
+++ app/concerns/tematica/tematica_base.rb
@@ -0,0 +1,30 @@
+# coding: UTF-8
+
+# require 'active_support/concern'
+
+module Tematica::TematicaBase
+  extend ActiveSupport::Concern
+
+  included do
+    validates :nombre, presence: true
+    validates :seccion_publi, presence: true
+  end
+
+    def to_param
+      "#{id}-#{ nombre..parameterize }"
+    end
+
+  module ClassMethods
+    def nombre(id)
+      Rails.cache.fetch("tematica-nombre-#{id}") do
+        Tematica::Tematica.find(id).nombre
+      end
+    end
+
+    def todas
+      Rails.cache.fetch("tematicas") do
+        Tematica::Tematica.all
+      end
+    end
+  end
+end
--- app/models/tematica/tematica.rb
+++ app/models/tematica/tematica.rb
@@ -1,25 +1,5 @@
 # coding: UTF-8
 
-module Tematica
-  class Tematica < ActiveRecord::Base
-
-    validates :nombre, presence: true
-    validates :seccion_publi, presence: true
-
-    def to_param
-      "#{id}-#{ nombre..parameterize }"
-    end
-
-    def self.nombre(id)
-      Rails.cache.fetch("tematica-nombre-#{id}") do
-        Tematica.find(id).nombre
-      end
-    end
-    
-    def self.todas
-      Rails.cache.fetch("tematicas") do
-        Tematica.all
-      end
-    end
-  end
+class Tematica::Tematica < ActiveRecord::Base
+  include Tematica::TematicaBase
 end

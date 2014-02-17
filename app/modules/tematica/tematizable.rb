c9e93baee37874a869745bd2019d16677901ccbe
Fernan2 <fernandorankia@gmail.com>
2014-02-17 17:47:47 +0100

    added fijar_tematicas! to tematizable

--- app/modules/tematica/tematizable.rb
+++ app/modules/tematica/tematizable.rb
@@ -9,6 +9,11 @@ module Tematica::Tematizable
     has_many :tematizaciones, as: :tematizable , class_name: 'Tematica::Tematizacion'
   end
 
+  def fijar_tematicas!(tematicas, grupo = '')
+    tematizaciones.where(tematizable_grupo: grupo).where("tematica_id NOT IN (?)", tematicas).delete_all
+    tematicas.each{ |tematica_id| asignar_tematica!(tematica_id, grupo) }
+  end
+
   def asignar_tematica!(tematica_id, grupo = '')
     busca_tematica(tematica_id, grupo) or tematizaciones.create!(tematica_id: tematica_id, tematizable_grupo: grupo)
   end

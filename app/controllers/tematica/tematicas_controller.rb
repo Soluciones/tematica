2490293bdf9ffe5ab7aada1ec88799e5bb27c648
Fernan2 <fernandorankia@gmail.com>
2014-06-06 14:53:35 +0200

    CRUD for Tematica using simpleform + inherited resources

--- app/controllers/tematica/tematicas_controller.rb
+++ app/controllers/tematica/tematicas_controller.rb
@@ -1,6 +1,7 @@
 # coding: UTF-8
 
 module Tematica
-  class TematicasController < ApplicationController
+  class TematicasController < InheritedResources::Base
+    before_filter :gestor_usr_required
   end
 end

495ace6c544cac7fd0bcf3186a8bfa10a88aa8c5
Fernan2 <fernandorankia@gmail.com>
2014-06-17 14:35:57 +0200

    new controller test -> needs use_route`

---
+++ spec/controllers/tematicas_controller_spec.rb
@@ -0,0 +1,17 @@
+# coding: UTF-8
+
+require 'spec_helper'
+
+describe Tematica::TematicasController, type: :controller do
+  describe 'destroy' do
+    context 'para una temática publicada' do
+      let!(:tematica) { FactoryGirl.create(:tematica, nombre: 'chorradas', publicado: true) }
+
+      it 'un destroy debe hacer un borrado lógico' do
+        delete :destroy, id: "#{ tematica.id }-chorradas", use_route: :tematicas
+        tematica.reload
+        expect(tematica).not_to be_publicado
+      end
+    end
+  end
+end

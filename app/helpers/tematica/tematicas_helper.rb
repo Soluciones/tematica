4f25ba05c9d3f5fb93faf5b2ac6d58ed6512891d
Fernan2 <fernandorankia@gmail.com>
2014-06-17 17:03:20 +0200

    hounding longline

--- app/helpers/tematica/tematicas_helper.rb
+++ app/helpers/tematica/tematicas_helper.rb
@@ -3,7 +3,8 @@
 module Tematica
   module TematicasHelper
     def link_borrar_o_recuperar(tematica)
-      return link_to('Borrar', tematica_path(tematica), method: :delete, confirm: "¿Borrar temática #{ tematica.nombre }?") if tematica.publicado
+      mensaje = "¿Borrar temática #{ tematica.nombre }?"
+      return link_to('Borrar', tematica_path(tematica), method: :delete, confirm: mensaje) if tematica.publicado
       link_to('Recuperar', recuperar_tematica_path(tematica), method: :post)
     end
   end

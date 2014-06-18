acdb4c27fd7733d91b10dfaa03b2ff6ebaa3f1d4
Fernan2 <fernandorankia@gmail.com>
2014-06-18 12:32:32 +0200

    Using dropdown as tematica selector

--- app/helpers/tematica/application_helper.rb
+++ app/helpers/tematica/application_helper.rb
@@ -1,4 +1,8 @@
 module Tematica
   module ApplicationHelper
+    def desplegable_tematicas
+      tematica_collection = Tematica.kind_of?(Module) ? Tematica::Tematica : Tematica
+      tematica_collection.publicado.all.collect{ |tematica| [tematica.nombre, tematica.id] }
+    end
   end
 end
--- app/models/tematica/tematica.rb
+++ app/models/tematica/tematica.rb
@@ -2,4 +2,5 @@
 
 class Tematica::Tematica < ActiveRecord::Base
   include Tematica::TematicaBase
+  scope :publicado, -> { where(publicado: true) }
 end
